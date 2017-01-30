53b59be9893d10f14fd5b91b47ae6a89154c4488
n-petrache <petrache.noemie@gmail.com>
2017-01-30 14:25:15 +0100

    nombres impairs

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      for ($num = 1; $num <= 300; $num = $num + 2){
+        echo "<p>$num</p>";
+      }
+    ?>
+  </body>
+</html>
